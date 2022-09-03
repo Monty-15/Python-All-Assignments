29d5b53c11f9607a8d6f78c802c72446349b3ffd
Kunj <kunjd26@gmail.com>
2022-09-03 23:05:32 +0530

    Assignment-01 Complete!

---
+++ Assignment-01/Assignment-01.py
@@ -0,0 +1,38 @@
+# 1. Write a command to get the Python version you are using.
+# python --version
+
+# 2. Write a python script to print Hello Python on the screen directly in the command line.
+print("Hello Python")
+
+# 3. Write a python script and store it in a file and execute the file from the command line.
+# python 01-prac.py
+
+# 4. Write a python script to print Hello Python on the screen using visual studio code.
+print("Hello Python")
+
+# 5. Write a python script to print Hello on the first line and Python on the second line
+print("Hello", "Python", sep='\n')
+
+# 6. Write a python script to print “MySirG” on the screen
+print("\"MySirG\"")
+
+# 7. Write a python script to print “Teacher’s Day” on the screen
+print("\"Teacher\'s Day\"")
+
+# 8. Write a python script to print the value of a variable. Variable contains your name as data.
+myName = "kunj"
+print("Your Name:", myName)
+
+# 9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+myName = "kunj"
+myAge = 18
+myQualification = "Diploma in IT"
+myExperience = 0
+print("Your Name:", myName)
+print("Your Age:", myAge)
+print("Your Qualification:", myQualification)
+print("Your Experience:", myExperience)
+
+# 10. Write a python script to print values of three variables, each in a new line. Variable contains integer values
+a, b, c = 10, 20, 30
+print(a, b, c, sep='\n')
